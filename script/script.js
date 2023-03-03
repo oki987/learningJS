b64c70002718e0c61c6acdaccb5962ed1ada41ec
Oskar Nykiel <oskar_nykiel@outlook.com>
2023-03-03 22:56:11 +0100

    1st session

---
+++ script/script.js
@@ -0,0 +1,30 @@
+// console.log('Hi!');
+// console.log('Hi!');
+
+// const name = 'John'
+
+// console.log(name);
+// console.log(name);
+
+// let age = 23
+
+// age = 25
+
+// let color
+
+// color = "blue"
+
+//------------------TASK 1------------------
+/*const name = "Oskar";
+let age = 21;
+let favDish = "Schabowy"*/
+
+//------------------TASK 2------------------
+const name = "Oskar"
+const animalName = "Puszek"
+
+console.log(`Cześć, jestem ${name}, a to jest ${animalName} - mój piesek.`)
+
+let animalAge = 5
+console.log(animalName+" ma już "+animalAge+" lat!")
+
