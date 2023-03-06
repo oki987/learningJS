6bc3bf2363c51230e921ea014030887da928716d
Oskar Nykiel <oskar_nykiel@outlook.com>
2023-03-06 18:46:33 +0100

    Introduction JS has ended

--- script/script.js
+++ script/script.js
@@ -20,11 +20,26 @@ let age = 21;
 let favDish = "Schabowy"*/
 
 //------------------TASK 2------------------
-const name = "Oskar"
+/*const name = "Oskar"
 const animalName = "Puszek"
 
 console.log(`Cześć, jestem ${name}, a to jest ${animalName} - mój piesek.`)
 
 let animalAge = 5
-console.log(animalName+" ma już "+animalAge+" lat!")
+console.log(animalName+" ma już "+animalAge+" lat!")*/
+
+//------------------TASK 3------------------
+const firstName = 'Tomek'
+let age = 12
+age = 18
+ 
+let favColor = 'niebieski' //nie używamy var!
+const favMeal = 'schabowy' //camelCase
+ 
+let currentCar // . przy nazwie zmiennej
+currentCar = 'Audi'
+ 
+firstName = 'Ania' //tej samej nazwy nie deklarujemy
+age = 24 //możemy użyć zmiennej wcześniejszej
+favColor = 'czerwony' //nie nazwywamy nazwy zmiennej po polsku
 
